91dc04c2ee675a2d9b08e24d45c3ef79e70577f8
zephrous <danielpoh3131@gmail.com>
2024-02-02 15:52:16 +0800

    update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -3,10 +3,14 @@ import Image from "next/image";
 export default function Hero(){
     return(
         <>
-            <section>
-                <h1>Welcome to FlashBite. Number 1 Food Ordering App In Malaysia.</h1>  
-                <p>Order Now To Get Your Foods Fast!</p>
-                <Image src={} alt={'Welcome'} />
+            <section className="grid grid-cols-2">
+                <div>
+                    <h1 className="text-4xl font-semibold">Welcome to FlashBite, No. Food App In Malaysia.</h1>  
+                    <p className="mt-4 text-gray-500">Order Now To Get Your Foods Fast!</p>
+                </div>
+                <div className="relative">
+                    <Image src={'/welcome.png'} layout={'fill'} objectFit={'contain'} alt={'Welcome'} />
+                </div>
             </section>
         </>
     )
