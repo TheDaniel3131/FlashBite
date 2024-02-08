ef886df4aadf19150ecfac2fbbe22545972e4097
zephrous <danielpoh3131@gmail.com>
2024-02-08 16:47:48 +0800

    update

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -2,16 +2,20 @@ import Link from "next/link"
 
 export default function Header(){
     return(
-        <>  
-            <header className="flex items-center justify-between">    
-                <Link className="text-primary text-4xl font-bold" href={''}>Flash<span className=" text-rose-600">Bite</span></Link>
+        <>
+            <header className="flex items-center justify-between text-white p-4">
+                <Link className="text-primary text-4xl font-bold" href={''}>
+                    Flash<span className="text-rose-600">Bite</span>
+                </Link>
                 <nav className="flex items-center gap-10 text-rose-300 font-bold">
                     <Link href={''}>Home</Link>
                     <Link href={''}>Foods</Link>
                     <Link href={''}>About Us</Link>
                     <Link href={''}>Contact Us</Link>
-                    <Link href={''}>FAQ</Link>  
-                    <Link href={''} className=" bg-rose-900 rounded-full text-white px-8 py-2">Login</Link>
+                    <Link href={''}>FAQ</Link>
+                    <Link href={''} className="bg-rose-900 rounded-full text-white px-8 py-2">
+                        Login
+                    </Link>
                 </nav>
             </header>
         </>
