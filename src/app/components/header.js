7bc918a1a53fbe0cab115f0668f223c63d707076
zephrous <danielpoh3131@gmail.com>
2024-02-08 16:50:01 +0800

    update

--- src/app/components/header.js
+++ src/app/components/header.js
@@ -7,13 +7,13 @@ export default function Header(){
                 <Link className="text-primary text-4xl font-bold" href={''}>
                     Flash<span className="text-rose-600">Bite</span>
                 </Link>
-                <nav className="flex items-center gap-10 text-rose-300 font-bold">
+                <nav className="flex items-center gap-12 text-rose-300 text-xl font-bold">
                     <Link href={''}>Home</Link>
                     <Link href={''}>Foods</Link>
                     <Link href={''}>About Us</Link>
                     <Link href={''}>Contact Us</Link>
                     <Link href={''}>FAQ</Link>
-                    <Link href={''} className="bg-rose-900 rounded-full text-white px-8 py-2">
+                    <Link href={''} className="bg-rose-900 rounded-full text-white px-10 py-2">
                         Login
                     </Link>
                 </nav>
