07dcff4b5a6361de95d47b533225295ce842d64d
zephrous <danielpoh3131@gmail.com>
2024-02-08 12:59:08 +0800

    update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,15 +1,16 @@
 import Image from "next/image";
+import Forward from "./icons/Forward";
 
 export default function Hero(){
     return(
         <>
             <section className="grid grid-cols-2 mt-12">
-                <div>
-                    <h1 className="text-4xl font-semibold">Welcome to FlashBite, No. 1 Food App In Malaysia.</h1>  
-                    <p className="mt-4 text-gray-500">Order Now To Get Your Foods Fast!</p>
-                        <div className="flex gap-4 mt-10">
-                            <button className=" bg-orange-300 px-9 py-3 text-white font-bold rounded-full">Learn More</button>
-                            <button className="bg-orange-500 font-bold px-9 py-3 rounded-full">Order Food</button>
+                <div className="py-4">
+                    <h1 className="mt-5 py-5 text-5xl font-semibold">Welcome to FlashBite, No. 1 Food App In Malaysia.</h1>  
+                    <p className="py-5 mt-5 text-gray-500">Order Now To Get Your Foods Fast!</p>
+                        <div className="flex gap-4">
+                            <button className="flex gap-2 bg-orange-300 px-6 py-3 text-white items-center font-bold rounded-full">Learn More <Forward /></button>
+                            <button className="flex gap-2 bg-orange-500 font-bold text-white uppercase items-center px-6 py-3 rounded-full">Order Food <Forward /></button>
                         </div>
                 </div>
 
