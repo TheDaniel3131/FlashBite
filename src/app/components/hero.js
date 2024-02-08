0a2d59c33e4ff6b353212303fb3a43d185286c43
zephrous <danielpoh3131@gmail.com>
2024-02-08 12:03:02 +0800

    some light adjustments

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -3,13 +3,13 @@ import Image from "next/image";
 export default function Hero(){
     return(
         <>
-            <section className="grid grid-cols-2">
+            <section className="grid grid-cols-2 mt-12">
                 <div>
                     <h1 className="text-4xl font-semibold">Welcome to FlashBite, No. 1 Food App In Malaysia.</h1>  
                     <p className="mt-4 text-gray-500">Order Now To Get Your Foods Fast!</p>
-                        <div>
-                            <button className=" bg-orange-300 px-16 py-2 text-white rounded-full">Learn More</button>
-                            <button className="rounded-full">Order Food</button>
+                        <div className="flex gap-4">
+                            <button className=" bg-orange-300 px-9 py-3 text-white font-bold rounded-full">Learn More</button>
+                            <button className="bg-orange-500 font-bold px-9 py-3 rounded-full">Order Food</button>
                         </div>
                 </div>
 
