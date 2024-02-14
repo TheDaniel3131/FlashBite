1f1c63dac751d110551a9b0dcfd0da880bc53bc3
zephrous <danielpoh3131@gmail.com>
2024-02-14 12:58:36 +0800

    update

--- src/app/components/mainmenu.js
+++ src/app/components/mainmenu.js
@@ -10,9 +10,15 @@ export default function MainMenu() {
             </div>
         </div>
 
-        <div className="text-right">
+        <div className="text-right relative">
             <h3 className="uppercase text-gray-500 text-xl font-semibold leading-loose">TRY OUT OUR LATEST MENU!</h3>      
             <h3 className="text-orange-500 font-bold text-4xl italic">FlashBite&rsquo;s Crispy Fried Chicken</h3>
+            <p className="absolute right-0 top-24 text-gray-700 mt-4 mr-5 w-1/2 h-40 leading-snug">
+                Deliciously crispy and flavorful fried chicken that will leave you wanting more. 
+                Made from locally sourced farm-fresh chicken, our dish is a perfect blend of 
+                traditional flavors and modern cooking techniques. Experience the taste of 
+                quality ingredients and indulge in the ultimate comfort food.
+            </p>
         </div>
     </section>
 
