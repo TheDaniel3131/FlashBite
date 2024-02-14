d9562a0c5c63c42d4c1befc3f0c90c21d6b9e020
zephrous <danielpoh3131@gmail.com>
2024-02-14 09:45:21 +0800

    update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import Forward from "./icons/Forward";
+import { Zap } from "react-feather";
 
 export default function Hero(){
     return(
         <>
             <section className="grid grid-cols-2 mt-12">
                 <div className="py-10">
-                    <h1 className="mt-5 py-5 text-6xl font-bold  leading-none">Welcome to <span className="">FlashBite! </span>No. 1 Food App In Malaysia.</h1>  
+                    <h1 className="mt-5 py-5 text-6xl font-bold leading-none">
+                        Welcome to <span className="flex items-center text-yellow-500">FlashBite <Zap size={48} className="ml-2" /></span>
+                        No. 1 Food App In Malaysia.
+                    </h1>
                     <p className="py-5 mb-5 tracking-widest text-xl text-gray-500">Order Now To Get Your Foods Fast!</p>
                         <div className="flex gap-4 text-sm mr-auto">
                             <button className="flex gap-2 bg-orange-300 px-6 py-3 text-white items-center font-bold rounded-full">Learn More <Forward /></button>
