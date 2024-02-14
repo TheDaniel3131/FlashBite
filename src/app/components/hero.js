1f4352b842b9828530425d0b2d9403f349f01232
zephrous <danielpoh3131@gmail.com>
2024-02-14 10:33:48 +0800

    update

--- src/app/components/hero.js
+++ src/app/components/hero.js
@@ -16,8 +16,8 @@ export default function Hero(){
                     </h1>
                     <p className="py-5 mb-5 tracking-widest text-xl text-gray-500">Order Now To Get Your Foods Fast!</p>
                         <div className="flex gap-4 text-sm mr-auto">
-                            <button className="flex gap-2 bg-orange-300 px-6 py-3 text-white items-center font-bold rounded-full">Learn More <Forward /></button>
-                            <button className="flex gap-2 bg-orange-500 font-bold text-white uppercase items-center px-6 py-3 rounded-full">Order Food <Forward /></button>
+                            <button className="flex gap-2 bg-orange-300 px-6 py-3 text-white items-center font-bold rounded-full text-xl">Learn More <Forward /></button>
+                            <button className="flex gap-2 bg-orange-500 font-bold text-white uppercase items-center px-6 py-3 rounded-full text-xl">Order Food <Forward /></button>
                         </div>
                 </div>
                 <div className="relative gap-3 mt-5 py-5">
