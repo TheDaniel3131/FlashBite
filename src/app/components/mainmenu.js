1a03749c366d34a8ac0179e741c7b25805cf9135
zephrous <danielpoh3131@gmail.com>
2024-02-21 17:35:45 +0800

    some progress

--- src/app/components/mainmenu.js
+++ src/app/components/mainmenu.js
@@ -1,26 +1,41 @@
-import React from 'react'
-import Image from 'next/image'
+import React from 'react';
+import Image from 'next/image';
 
 export default function MainMenu() {
   return (
-    <section className="relative mt-10 py-10">
-        <div className="absolute left-0 right-0">
-            <div className="h-80 w-80 absolute left-0 top-0"> 
-                <Image src={'/friedchicken.png'} layout={'fill'} objectFit={'contain'} alt={'Fried Chicken'} className="rounded-full"/>
-            </div>
-        </div>
-
+    <section className="relative mt-20 py-10">
         <div className="text-right relative">
             <h3 className="uppercase text-gray-500 text-xl font-semibold leading-loose mt-10 mr-5">TRY OUT OUR LATEST MENU!</h3>      
             <h3 className="text-orange-500 font-bold text-4xl italic mr-5">FlashBite&rsquo;s Crispy Fried Chicken</h3>
-            <p className="absolute right-0 top-24 text-gray-700 mt-4 mr-5 w-1/2 h-40 leading-snug">
+            <p className="absolute right-0 top-24 text-gray-700 mt-4 mr-5 w-2/3 h-40 leading-snug">
                 Deliciously crispy and flavorful fried chicken that will leave you wanting more. 
                 Made from locally sourced farm-fresh chicken, our dish is a perfect blend of 
                 traditional flavors and modern cooking techniques. Experience the taste of 
                 quality ingredients and indulge in the ultimate comfort food.
             </p>
         </div>
-    </section>
+        
 
-  )
+      <div className="text-center mb-20 mt-60">
+        <h3 className="uppercase text-gray-500 font-semibold leading-4">Check out</h3>
+        <h2 className="text-primary font-bold text-4xl italic">MORE MENU</h2>
+      </div>
+
+      <div className="grid grid-cols-3 gap-4">
+        {/* Example grid item */}
+        <div className="bg-gray-300 p-4 rounded-lg text-center">
+          <Image src="" alt="Food" width={100} height={100}/>
+          <h4 className=""></h4>
+          <p className="text-gray-500 text-sm">
+            Lorem ipsum dolor sit amet, consectetur adipiscing elit. In hac habitasse platea dictumst.
+          </p>
+        </div>
+      </div>
+
+      {/* Image */}
+      <div className="absolute left-0 top-12 h-80 w-80">
+        <Image src={'/friedchicken.png'} layout={'fill'} objectFit={'contain'} alt={'Fried Chicken'} className="rounded-full"/>
+      </div>
+    </section>
+  );
 }
