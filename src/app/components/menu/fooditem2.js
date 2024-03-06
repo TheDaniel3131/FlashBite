4dde49dab4a1bfeb022314e0948885560787d2f3
zephrous <danielpoh3131@gmail.com>
2024-03-06 16:55:12 +0800

    update

--- src/app/components/menu/fooditem2.js
+++ src/app/components/menu/fooditem2.js
@@ -3,10 +3,10 @@ import Image from 'next/image';
 export default function FoodItem2(){
     return( 
         <div className="p-6 rounded-lg text-center items-center justify-center hover:bg-rose-200">
-          <Image src={'/nasilemak.jpg'} alt="Food" width={350} height={350}/>
-          <h4 className="font-bold text-2xl my-5 mb-5">Tradisional Nasi Lemak</h4>
+          <Image src={'/charkweyteow.png'} alt="Food" width={350} height={350}/>
+          <h4 className="font-bold text-2xl my-5 mb-5">Char Kwey Teow</h4>
           <p className="text-gray-500 text-sm mb-5">
-            A traditional Malay fragrant rice dish cooked in coconut milk and pandan leaf.
+            A popular noodle dish from Malaysia, made from flat rice noodles stir-fried over high heat with light and dark soy sauce, chilli, prawns, cockles, egg, bean sprouts, and Chinese chives.
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
