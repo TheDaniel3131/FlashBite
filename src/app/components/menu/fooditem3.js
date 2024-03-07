2c46d23715c265a3a1b8989040699c330a8584fe
zephrous <danielpoh3131@gmail.com>
2024-03-07 08:35:01 +0800

    update

--- src/app/components/menu/fooditem3.js
+++ src/app/components/menu/fooditem3.js
@@ -3,7 +3,7 @@ import Image from 'next/image';
 export default function FoodItem3(){
     return( 
         <div className="p-6 rounded-lg text-center items-center justify-center hover:bg-rose-200">
-          <Image src={'/roticanai.jpg'} alt="Food" width={350} height={350}/>
+          <Image src={'/roticanai.jpg'} alt="Food" width={350} height={350} objectFit="cover" objectPosition="center"/>
           <h4 className="font-bold text-2xl my-5 mb-5">Roti Canai</h4>
           <p className="text-gray-500 text-sm mb-5">
             Roti canai is a type of Indian-influenced flatbread found in Malaysia, Brunei, Indonesia and Singapore.
