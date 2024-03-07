6bd95125355cb4e093c28095b0306616f2e99a68
zephrous <danielpoh3131@gmail.com>
2024-03-07 08:49:26 +0800

    asam laksa

--- src/app/components/menu/fooditem4.js
+++ src/app/components/menu/fooditem4.js
@@ -3,10 +3,11 @@ import Image from 'next/image';
 export default function FoodItem4(){
     return( 
         <div className="p-6 rounded-lg text-center items-center justify-center hover:bg-rose-200">
-          <Image src={'/nasilemak.jpg'} alt="Food" width={350} height={350}/>
-          <h4 className="font-bold text-2xl my-5 mb-5">Tradisional Nasi Lemak</h4>
+          <Image src={'/asamlaksa.jpg'} alt="Food" width={350} height={350}/>
+          <h4 className="font-bold text-2xl my-5 mb-5">Asam Laksa</h4>
           <p className="text-gray-500 text-sm mb-5">
-            A traditional Malay fragrant rice dish cooked in coconut milk and pandan leaf.
+            Asam laksa is a sour, fish-based soup. Asam is Malay for &quot;sour&quot;, and the soup is often served with either mackerel or sardines. 
+            It is a popular dish in Malaysia, and also in Indonesia and Singapore. 
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
