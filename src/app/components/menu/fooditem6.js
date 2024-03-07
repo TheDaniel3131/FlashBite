52083da326ca342152f4201eca76f5e5bced4527
zephrous <danielpoh3131@gmail.com>
2024-03-07 09:03:25 +0800

    change some description texts

--- src/app/components/menu/fooditem6.js
+++ src/app/components/menu/fooditem6.js
@@ -7,7 +7,7 @@ export default function FoodItem6(){
           <h4 className="font-bold text-2xl my-5 mb-5">Hainan Chicken Rice</h4>
           <p className="text-gray-500 text-sm mb-5">
             Hainanese chicken rice is a dish of poached chicken and seasoned rice, served with chilli sauce and usually comes with cucumber garnishes. 
-            Chicken rice is most commonly associated with Hainanese, Malaysian and Singaporean cuisines, although it is also commonly sold in Thailand and Vietnam.
+            Chicken rice is most commonly associated with Hainanese, Malaysian and Singaporean cuisines, it is also popular in Thailand and Vietnam. 
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
