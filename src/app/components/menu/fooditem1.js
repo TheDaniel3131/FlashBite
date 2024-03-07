8fd42f08f3352ff13e9267321937ee569fa1efb8
zephrous <danielpoh3131@gmail.com>
2024-03-07 08:27:52 +0800

    added description

--- src/app/components/menu/fooditem1.js
+++ src/app/components/menu/fooditem1.js
@@ -6,7 +6,8 @@ export default function FoodItem1(){
           <Image src={'/nasilemak.jpg'} alt="Food" width={350} height={350}/>
           <h4 className="font-bold text-2xl my-5 mb-5">Tradisional Nasi Lemak</h4>
           <p className="text-gray-500 text-sm mb-5">
-            A traditional Malay fragrant rice dish cooked in coconut milk and pandan leaf.
+            A traditional Malay fragrant rice dish cooked in coconut milk and pandan leaf. 
+            It is commonly found in Malaysia, where it is considered the national dish.
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
