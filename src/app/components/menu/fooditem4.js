8e2363ee2a4b2448973e49a6fa4a59a348899b76
zephrous <danielpoh3131@gmail.com>
2024-03-07 09:02:26 +0800

    update text

--- src/app/components/menu/fooditem4.js
+++ src/app/components/menu/fooditem4.js
@@ -6,8 +6,8 @@ export default function FoodItem4(){
           <Image src={'/asamlaksa.jpg'} alt="Food" width={350} height={350}/>
           <h4 className="font-bold text-2xl my-5 mb-5">Asam Laksa</h4>
           <p className="text-gray-500 text-sm mb-5">
-            Asam laksa is a sour, fish-based soup. Asam is Malay for &quot;sour&quot;, and the soup is often served with either mackerel or sardines. 
-            It is a popular dish in Malaysia, and also in Indonesia and Singapore. 
+            Asam laksa is a sour, fish-based soup. Asam is Malay for &quot;sour&quot;, and the soup is often served with either mackerel or sardines.
+            As for &quot;Laksa&quot; is a spicy noodle soup from the Peranakan culture, which is a merger of Chinese and Malay elements found in Malaysia and Singapore. 
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
