a580f26bcbcc6576101b74ebdeaab977c9bf542d
zephrous <danielpoh3131@gmail.com>
2024-03-07 08:58:01 +0800

    update items

--- src/app/components/menu/fooditem6.js
+++ src/app/components/menu/fooditem6.js
@@ -3,10 +3,11 @@ import Image from 'next/image';
 export default function FoodItem6(){
     return( 
         <div className="p-6 rounded-lg text-center items-center justify-center hover:bg-rose-200">
-          <Image src={'/nasilemak.jpg'} alt="Food" width={350} height={350}/>
-          <h4 className="font-bold text-2xl my-5 mb-5">Tradisional Nasi Lemak</h4>
+          <Image src={'/hainanchickenrice.jpg'} alt="Food" width={350} height={350}/>
+          <h4 className="font-bold text-2xl my-5 mb-5">Hainan Chicken Rice</h4>
           <p className="text-gray-500 text-sm mb-5">
-            A traditional Malay fragrant rice dish cooked in coconut milk and pandan leaf.
+            Hainanese chicken rice is a dish of poached chicken and seasoned rice, served with chilli sauce and usually comes with cucumber garnishes. 
+            Chicken rice is most commonly associated with Hainanese, Malaysian and Singaporean cuisines, although it is also commonly sold in Thailand and Vietnam.
           </p>
           <button className="bg-primary font-bold shadow-xl text-white rounded-full px-8 py-2">Order for RM12.50</button>
         </div>   
