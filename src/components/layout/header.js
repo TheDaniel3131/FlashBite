a1d6a7813fc66f39c16e5d57f96b56d14d038037
zephrous <danielpoh3131@gmail.com>
2024-03-11 16:53:58 +0800

    header stuffs again

--- src/components/layout/header.js
+++ src/components/layout/header.js
@@ -8,13 +8,15 @@ export default function Header(){
                 <Link className="text-primary text-5xl font-bold" href={''}>
                     Flash<span className="text-rose-600">Bite</span>
                 </Link>
-                <nav className="flex items-center gap-12 text-rose-400 text-xl font-semibold tracking-wider justify-between">
-                    <Link href={''}>Home</Link>
-                    <Link href={''}>Foods</Link>
-                    <Link href={''}>About Us</Link>
-                    <Link href={''}>Contact Us</Link>
-                    <Link href={''}>FAQ</Link>
-                    <Link href={'/login'} className="flex gap-2 text-2xl bg-rose-900 rounded-full text-white px-10 py-2 mr-auto">
+                <nav className="flex items-center gap-12 text-rose-400 text-xl justify-between">
+                    <div className="flex items-center gap-12 text-rose-400 text-xl justify-between font-semibold">
+                        <Link href={''}>Home</Link>
+                        <Link href={''}>Foods</Link>
+                        <Link href={''}>About Us</Link>
+                        <Link href={''}>Contact Us</Link>
+                        <Link href={''}>FAQ</Link>
+                    </div>
+                    <Link href={'/login'} className="flex gap-2 text-2xl bg-rose-900 rounded-full text-white font-bold tracking-tight px-10 py-2 mr-auto">
                         Login<LogIn size={36} />
                     </Link>
                 </nav>
