1a6839834554a25f8823709ebbaa8c7f47b99442
zephrous <danielpoh3131@gmail.com>
2024-03-11 12:39:34 +0800

    update

--- src/app/components/layout/header.js
+++ src/app/components/layout/header.js
@@ -1,20 +1,21 @@
 import Link from "next/link"
+import { LogIn } from 'lucide-react';
 
 export default function Header(){
     return(
         <>
-            <header className="flex items-center justify-between text-white p-4">
-                <Link className="text-primary text-4xl font-bold" href={''}>
+            <header className="flex items-center justify-between text-white py-4">
+                <Link className="text-primary text-5xl font-bold" href={''}>
                     Flash<span className="text-rose-600">Bite</span>
                 </Link>
-                <nav className="flex items-center gap-12 text-rose-300 text-xl font-bold">
+                <nav className="flex items-center gap-12 text-rose-300 text-xl font-bold justify-between">
                     <Link href={''}>Home</Link>
                     <Link href={''}>Foods</Link>
                     <Link href={''}>About Us</Link>
                     <Link href={''}>Contact Us</Link>
                     <Link href={''}>FAQ</Link>
-                    <Link href={''} className="bg-rose-900 rounded-full text-white px-10 py-2">
-                        Login
+                    <Link href={''} className="flex gap-2 text-2xl bg-rose-900 rounded-full text-white px-10 py-2 mr-auto">
+                        Login<LogIn size={36} />
                     </Link>
                 </nav>
             </header>
