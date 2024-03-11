15335f95068511986e7fcc5037206c6c969e9da6
zephrous <danielpoh3131@gmail.com>
2024-03-11 16:25:43 +0800

    update

--- src/app/page.js
+++ src/app/page.js
@@ -51,7 +51,7 @@ export default function Home(){
           </a>
         </div>
       </section>
-      <footer className="border-t p-10 text-center text-slate-500">
+      <footer className="border-t-2 p-10 text-center text-slate-500">
         <p>&copy; 2024 FlashBites. All rights reserved.</p>
       </footer>
     </>
