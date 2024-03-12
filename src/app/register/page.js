788c467efa119574222856c8d145644e6b847ad7
zephrous <danielpoh3131@gmail.com>
2024-03-12 12:17:11 +0800

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -6,11 +6,11 @@ export default function Register(){
             </div>
             <form className="flex flex-col items-left bg-slate-100 p-20 rounded-3xl">
                 <div className="flex items-center mb-5">
-                    <h1 className="font-semibold">Email: &nbsp;</h1>
+                    <h1 className="font-normal tracking-wider text-lg">Email: &nbsp;</h1>
                     <input type="email" placeholder="Enter Your Email Address" className="px-4 py-2"/>
                 </div>
                 <div className="flex items-center mb-10">
-                    <h1 className="font-semibold">Password: &nbsp;</h1>
+                    <h1 className="font-normal tracking-wider text-lg">Password: &nbsp;</h1>
                     <input type="password" placeholder="Enter Your Password" className="px-4 py-2"/>
                 </div>
                 <button type="submit" className="bg-slate-500 py-3 px-10 rounded-full font-bold text-slate-100">Register Now</button>
