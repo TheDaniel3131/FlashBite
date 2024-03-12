73a2ba6ba000b6a78b867372b262d38f46bd6de4
zephrous <danielpoh3131@gmail.com>
2024-03-12 11:54:56 +0800

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -1,5 +1,15 @@
 export default function Register(){
     return(
-        <div>Member Registration</div>
+        <section className="flex flex-col items-center justify-between mt-5 py-10">
+            <div className="mb-16">
+                <h1 className="font-bold text-center text-priamry text-4xl">Member Registration</h1>
+            </div>
+            <form className="">
+                Email: &nbsp;<input type="email" placeholder="email"/>
+                Password: &nbsp;<input type="password" placeholder="password"/>
+                <button type="submit" className="bg-slate-500 py-5 px-10 rounded-full font-bold text-slate-100">Register Now</button>
+
+            </form>
+        </section>
     )
 }
