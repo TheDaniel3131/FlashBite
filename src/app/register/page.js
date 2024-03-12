b689900a78e99e282d4aef4eee1a9029845a82ea
zephrous <danielpoh3131@gmail.com>
2024-03-12 12:32:23 +0800

    update

--- src/app/register/page.js
+++ src/app/register/page.js
@@ -3,9 +3,9 @@ export default function Register(){
         <section className="flex flex-col items-center justify-between mt-5 py-10">
             <div className="flex flex-col mb-10">
                 <h1 className="font-bold items-center text-center text-slate-800 text-4xl">Member Registration</h1>
-            </div>  
-            <form className="flex flex-col items-left bg-slate-100 p-20 rounded-3x w-full mx-auto max-w-xl">
-                <div className="flex items-center mb-5">
+            </div>      
+            <form className="flex flex-col text-center items-center bg-slate-100 p-20 rounded-3x w-full mx-auto max-w-xl">
+                <div className="flex items-center mb-6">
                     <h1 className="font-normal tracking-wider text-lg">Email: &nbsp;</h1>
                     <input type="email" placeholder="Enter Your Email Address" className="px-10 py-2"/>
                 </div>
@@ -13,7 +13,7 @@ export default function Register(){
                     <h1 className="font-normal tracking-wider text-lg">Password: &nbsp;</h1>
                     <input type="password" placeholder="Enter Your Password" className="px-6 py-2"/>
                 </div>
-                <button type="submit" className="bg-slate-500 py-3 px-10 rounded-full font-bold text-slate-100">Register Now</button>
+                <button type="submit" className="bg-slate-500 py-3 px-12 rounded-full font-bold text-slate-100 tracking-normal">Register Now</button>
             </form>
         </section>
     )
