c73ac41d3b5037bc8d7844faa19ff95eebc6384f
zephrous <danielpoh3131@gmail.com>
2024-03-13 16:22:50 +0800

    update

--- src/app/models/user.js
+++ src/app/models/user.js
@@ -2,9 +2,13 @@ import { Schema } from 'mongoose';
 
 const UserSchema = new Schema({
     email: {type: String, required: true, unique: true},
-    password: {type: String, required: true, 
+    password: {
+        type: String, 
+        required: true, 
         validate: () => {
-           this.password;
+           if (!pass?.length || pass.length < 5){
+            new Error('Password must be minimum 5 characters.')
+           }
         },
     },
 })
