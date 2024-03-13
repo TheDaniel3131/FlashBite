c87b4b90f79de46742435cc46cfa533f22a80bce
zephrous <danielpoh3131@gmail.com>
2024-03-13 16:52:07 +0800

    update

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -1,6 +1,11 @@
-import moongose from 'mongoose';
+import mongoose from 'mongoose';
 import { User } from '@/app/models/user';
 
-export function POST(req){
-     return Response.json('This is a message from Flashbite API!');
+export async function POST(req){
+     const body = await req.json();
+     mongoose.connect(process.env.MONGO_URI);
+     const createdUser = await User.create(body);
+     return Response.json(createdUser);
+     // Default testing response
+     // return Response.json('This is a message from Flashbite API!');
 }
