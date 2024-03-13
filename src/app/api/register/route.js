aeb1377bc0a584267bc3f254c752df12f3207073
zephrous <danielpoh3131@gmail.com>
2024-03-13 16:58:24 +0800

    typo

--- src/app/api/register/route.js
+++ src/app/api/register/route.js
@@ -3,7 +3,7 @@ import { User } from "@/app/models/user";
 
 export async function POST(req){
      const body = await req.json();
-     mongoose.connect(process.env.MONGO_URI);
+     mongoose.connect(process.env.MONGO_URL);
      const createdUser = await User.create(body);
      return Response.json(createdUser);
      // Default testing response
