12bb0350519543bb73a38976554144f5c9d86d66
zephrous <danielpoh3131@gmail.com>
2024-03-13 15:08:44 +0800

    update

---
+++ src/app/models/user.js
@@ -0,0 +1,11 @@
+import { Schema } from 'mongoose';
+
+const UserSchema = new Schema({
+    email: {type: String, required: true, unique: true},
+    password: {type: String, required: true, 
+        validate:{
+            message: '',
+            
+    }} 
+
+})
