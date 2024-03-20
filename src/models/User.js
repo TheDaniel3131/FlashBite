cf60145fe13430386ab9ccff86775cfb8e28ae60
zephrous <danielpoh3131@gmail.com>
2024-03-20 14:56:53 +0800

    update

--- src/models/User.js
+++ src/models/User.js
@@ -5,17 +5,23 @@ const UserSchema = new Schema({
     password: {
         type: String, 
         required: true, 
-        validate: function() {
-           if (!this.password?.length || this.password.length < 5){
+        validate: pass => {
+           if (!pass?.length || pass.length < 5){
             throw new Error('Password must be minimum 5 characters.')
            }
+        // validate: function() {
+        //    if (!this.password?.length || this.password.length < 5){
+        //     throw new Error('Password must be minimum 5 characters.')
+        //    }
         },
     },
 }, {timestamps: true});
 
-UserSchema.pre('save', (next, ...rest) => {
-    console.log(rest);
-    next();
-})
+// Not working because no document
+
+// UserSchema.pre('save', (next, ...rest) => {
+//     console.log(rest);
+//     next();
+// })
 
 export const User = models?.User || model('User', UserSchema);
