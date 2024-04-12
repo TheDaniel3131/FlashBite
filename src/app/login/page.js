afd71a3588b3bd4c1bf3fb8f0a2875fd91596b53
zephrous <danielpoh3131@gmail.com>
2024-04-13 01:21:15 +0800

    update

---
+++ src/app/login/page.js
@@ -0,0 +1,7 @@
+export default function Login(){
+    return(
+        <section className="flex flex-col items-center justify-between mt-5 py-10">
+            <h1 className="font-bold items-center text-center text-slate-800 text-4xl">User Login</h1>
+        </section>
+    );
+}
