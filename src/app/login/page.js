551b5c953868138162924fefb67a184dc9fa66cd
zephrous <danielpoh3131@gmail.com>
2024-04-13 01:36:08 +0800

    update

--- src/app/login/page.js
+++ src/app/login/page.js
@@ -12,8 +12,8 @@ export default function Login(){
     
     return(
         <section className="flex flex-col items-center justify-between mt-5 py-10">
-            <h1 className="font-bold items-center text-center text-slate-800 text-4xl">User Login</h1>
-            <form className="max-w-xs mx-auto">
+            <h1 className="font-bold items-center text-center text-slate-800 text-5xl">User Login</h1>
+            <form className="max-w-xs mx-auto py-10 mt-5">
                 <input type="email" placeholder="Enter Your Email Address" value={email} disabled={false} onChange={ev => setEmail(ev.target.value)} className="px-6 py-2"/>
                 <input type="password" placeholder="Enter Your Password" value={password} disabled={false} onChange={ev => setPassword(ev.target.value)} className="px-6 py-2"/>
             </form>
